daa5951728e4151e18044ca0b3d86f4a438bd528
Jose Campos <zeedfisica@gmail.com>
2026-01-12 08:46:24 -0300

    Configure PWA with Serwist and Icons

--- next.config.mjs
+++ next.config.mjs
@@ -8,4 +8,11 @@ const nextConfig = {
   },
 }
 
-export default nextConfig
+import withSerwistInit from "@serwist/next"
+
+const withSerwist = withSerwistInit({
+  swSrc: "app/sw.ts",
+  swDest: "public/sw.js",
+})
+
+export default withSerwist(nextConfig)
